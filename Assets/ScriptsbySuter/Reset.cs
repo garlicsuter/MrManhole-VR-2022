175345b609799673bd805e8b5abb293417f6499e
Garlicsuter <markasuter@gmail.com>
2023-03-24 16:45:13 -0400

    Prep for final animation of white ring

--- Assets/ScriptsbySuter/Reset.cs
+++ Assets/ScriptsbySuter/Reset.cs
@@ -6,6 +6,10 @@ public class Reset : MonoBehaviour
 {
     public GameObject startButton;
     public GameObject manholeCover;
+    public GameObject Road_KerfandManhhole;
+    public GameObject Road__w_Larger_DonutHole;
+    public GameObject Road_w_LargerKerfHole;
+    public GameObject customRoad_Old_Small;
     public GameObject Q1;
     public GameObject Q2;
     public GameObject Q3;
@@ -17,6 +21,11 @@ public class Reset : MonoBehaviour
     {
         startButton.SetActive(true);
         manholeCover.SetActive(true);
+        Road_KerfandManhhole.SetActive(false);
+        Road__w_Larger_DonutHole.SetActive(false);
+        Road_w_LargerKerfHole.SetActive(false);
+        customRoad_Old_Small.SetActive(true);
+
         Q1.SetActive(false);
         Q2.SetActive(false);
         Q3.SetActive(false);
